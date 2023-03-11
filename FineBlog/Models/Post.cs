10acf0f0b27261e54110683c1f021c41848e5182
chtsuser <chtsuser@DESKTOP-8N5EE68>
2023-03-11 23:28:40 +0800

    加入專案檔案。

---
+++ FineBlog/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace FineBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+
+        //relation
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
+        public string? Slug { get; set; } //slug別名
+    }
+}
