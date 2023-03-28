1a46e54877444d8d3c632e503f776c5c7b51f749
chtsuser <chtsuser@DESKTOP-8N5EE68>
2023-03-29 00:15:32 +0800

    Part15,新增 Post View

--- FineBlog/ViewModels/CreatePostVM.cs
+++ FineBlog/ViewModels/CreatePostVM.cs
@@ -1,18 +1,17 @@
-﻿namespace FineBlog.Models
+﻿using FineBlog.Models;
+
+namespace FineBlog.ViewModels
 {
-    public class Post
+    public class CreatePostVM
     {
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? ShortDescription { get; set; }
 
-        //relation
         public string? ApplicationUserId { get; set; }
-        public ApplicationUser? ApplicationUser { get; set; }
-        public DateTime CreateDate { get; set; } = DateTime.Now;
         public string? Description { get; set; }
-        public string? Slug { get; set; } //slug別名
         public string? ThumbnailUrl { get; set; }
-       
+
+        public IFormFile? Thumbnal { get; set; }
     }
 }
